03e7d4b41c32169d2eb4d9840daa61ba2964405e
Nika <Nika@users-MacBook-Pro.local>
2024-11-13 13:57:49 +0400

    feat(header): add Hero component with title and subtitle

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import styles from "../../styles/Hero.module.css";
+
+const Hero = () => {
+  return (
+    <div className={styles.titleWrapper}>
+      <h2 className={styles.title}>
+        Start your unforgettable journey with us.
+      </h2>
+      <span className={styles.subTitle}>
+        The best travel for your jouney begins now
+      </span>
+    </div>
+  );
+};
+
+export default Hero;
