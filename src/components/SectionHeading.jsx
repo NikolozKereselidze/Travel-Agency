1260605db39696d70e2162f0c679c83c926aa4f7
Nika <Nika@users-MacBook-Pro.local>
2024-11-23 16:55:41 +0400

    style: update sectionHeading border

--- src/components/SectionHeading.jsx
+++ src/components/SectionHeading.jsx
@@ -4,8 +4,10 @@ import styles from "../styles/SectionHeading.module.css";
 const SectionHeading = ({ title, description }) => {
   return (
     <div className={styles.infoWrapper}>
-      <h3 className={styles.title}>{title}</h3>
-      <span className={styles.description}>{description}</span>
+      <div className={styles.infoContainer}>
+        <h3 className={styles.title}>{title}</h3>
+        <span className={styles.description}>{description}</span>
+      </div>
     </div>
   );
 };
