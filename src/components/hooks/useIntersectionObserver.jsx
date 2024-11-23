c1e0c91bed2d48c93048dd9d67bd38f14b0682a8
Nika <Nika@users-MacBook-Pro.local>
2024-11-23 16:56:29 +0400

    feat: create intersectionObserver hook

---
+++ src/components/hooks/useIntersectionObserver.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef, useState } from "react";
+
+const useIntersectionObserver = () => {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsIntersecting(entry.isIntersecting);
+    });
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  return [ref, isIntersecting];
+};
+
+export default useIntersectionObserver;
