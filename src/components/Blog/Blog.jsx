755131d4d6f9792642ef33b977f97f9688f67236
Nika <Nika@users-MacBook-Pro.local>
2024-11-25 17:33:43 +0400

    style: make Blog section responsive

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -31,14 +31,15 @@ const Blog = () => {
             Beautiful Italy <br></br> Let's travel
           </h3>
           <p>
-            But I must explain to you how all this mistaken idea of denouncing
-            pleasure and praising pain was born and I will give you a complete
-            account of the system and expound the actual teachings of the great
-            explorer of the truth, the master- builder of human happiness. No
-            one rejects, dislike, or avoids plasure itself, because it is
-            pleasure, but because those who do not know how to pursue pleasure
-            rationally encounter consequences that are extremly painful. Nor
-            again is there anyone who loves or pursues.
+            Embark on a journey through the heart of Italy with our blog,
+            Beautiful Italy: Let's Travel. Discover the charm of cobblestone
+            streets, the aroma of freshly baked pizza, and the allure of rolling
+            Tuscan hills. From iconic landmarks like the Colosseum and Venice's
+            canals to hidden villages waiting to be explored, we bring you the
+            magic of Italy through captivating stories and insider tips. Whether
+            you're dreaming of a getaway or planning your next adventure, let us
+            inspire you to experience the beauty and culture of Italy like never
+            before!
           </p>
           <button className={styles.readMore}>Read More</button>
         </div>
